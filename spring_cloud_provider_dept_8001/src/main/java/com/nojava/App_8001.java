cbd8ef694f28e98a4b34491b9b2989cc600348d7
lixiaofeng_global <lixiaofeng_global@qq.com>
2019-11-21 22:01:40 +0800

    微服务 服务提供者 spring_cloud_provider_dept_8001

---
+++ spring_cloud_provider_dept_8001/src/main/java/com/nojava/App_8001.java
@@ -0,0 +1,20 @@
+package com.nojava;
+
+import com.nojava.util.TestData;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ */
+@SpringBootApplication
+public class App_8001
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App_8001.class,args);
+    }
+
+}
