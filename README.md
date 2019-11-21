f064316c003dd877895bee6b6c0cacf613c26adb
lixiaofeng_global <lixiaofeng_global@qq.com>
2019-11-21 20:29:35 +0800

    公共模块提交

--- README.md
+++ README.md
@@ -3,7 +3,21 @@
 go...
 ----------------------
 **1.从开始建项目开始学习**
-
-1.1创建普通的maven项目
-
-1.1.1修改pom文件
+    
+    1.1创建普通的maven项目 也就pom父工程
+    1.1.1删除src目录 修改pom文件，详见partent pom文件
+        因为我们要学习springclould 所以引入了springclould(spring-cloud-dependencies)
+**2.公共子模块**
+    
+    2.1 测试使用下lombok 代码中可以省略无参有参构造 get set 方法 tostring。（试试玩的，生产上还是不要使用）
+    2.2 mvn clean  mvn install 后给其他模块引用，达到通用目的
+    eg:        <dependency><!--引入自己api通用包，可以使用api的实体-->
+                   <artifactId>spring_cloud_parent</artifactId>
+                   <groupId>com.nojava</groupId>
+                   <version>${project.version}</version>
+               </dependency>
+**3.创建一个服务提供者**
+    
+    约定》配置》编码    
+    
+    
---
+++ spring_cloud_api/src/main/java/com/nojava/Dept.java
@@ -0,0 +1,37 @@
+package com.nojava;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: nojava
+ * Date: 2019-11-21
+ * Time: 0:29
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)    //链式写法
+public class Dept implements Serializable {
+
+    private Long deptno;
+
+    private String dname;
+
+    private String db_source;
+
+
+    public static void main(String[] args) {
+        Dept dept = new Dept();
+        ////链式写法
+        dept.setDeptno(111L).setDname("lixiaofeng").setDb_source("11");
+        System.out.println(dept.toString());
+    }
+
+}
